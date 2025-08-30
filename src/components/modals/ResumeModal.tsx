9884dfde2abe64579e7729664ce3f100b6ba6350
poonyawat0511 <poonyawat0931@gmail.com>
2025-08-30 23:45:37 +0700

    back to 1st resume

--- src/components/modals/ResumeModal.tsx
+++ src/components/modals/ResumeModal.tsx
@@ -16,15 +16,25 @@ export default function ResumeModal({
   onClose: () => void;
 }) {
   return (
-    <Modal isOpen={isOpen} onClose={onClose} backdrop="blur" className="bg-black rounded-xl">
+    <Modal isOpen={isOpen} onClose={onClose} backdrop="blur">
       <ModalContent className="max-w-5xl w-full">
-        <>
-          <iframe
-            src="/pdf/CV_Poonyawat.pdf"
-            className="w-full h-[80vh] rounded-xl"
-            title="Resume PDF"
-          />
-        </>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">My Resume</ModalHeader>
+            <ModalBody>
+              <iframe
+                src="/pdf/CV_Poonyawat.pdf"
+                className="w-full h-[70vh] border"
+                title="Resume PDF"
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="light" onPress={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </>
+        )}
       </ModalContent>
     </Modal>
   );
